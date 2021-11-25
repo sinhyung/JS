7bdf14a6478a83233f97b9d41d2a21f3d46a283e
Kimsinhyung <cutebap0120@naver.com>
2021-11-25 18:28:31 +0900

    [New] First

---
+++ njs1.js
@@ -0,0 +1,3 @@
+document.write ("njs3.js");
+document.write ("<div style='color:green;font-size:24px;'> 외부 자바스크립트 파일 </div>");
+document.write( "<script src= 'njs2.js'> </script> " );
---
+++ njs3.js
@@ -0,0 +1,3 @@
+document.write ("njs3.js");
+document.write ("<div style='color:green;font-size:16px;'> 외부 자바스크립트 파일 </div>");
+alert ('Nested Script File : 내포된 자바스크립트')
